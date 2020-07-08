98c16e4673107b39ace895b9d937ab07e1a614ab
GithungoIan <githungoian53@gmail.com>
2020-07-08 11:02:44 +0300

    Generating random number

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+extern crate rand;
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess a number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret numer is : {}", secret_number);
     println!("Enter your guess");
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failled to read line");
-        
     println!("You guessed: {}", guess);
 }

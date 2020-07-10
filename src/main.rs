8dced243647853f8a1059bf05d6da3ae3fe97b7a
GithungoIan <githungoian53@gmail.com>
2020-07-10 12:53:14 +0300

    Delete the secret_number print statement

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use std::io;
 fn main() {
     println!("Guess a number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret numer is : {}", secret_number);
     loop {
         println!("Enter your guess");
         let mut guess = String::new();
